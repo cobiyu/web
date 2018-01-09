7a4328bdb4ad8d9ce51a845ec8a5468b314d36a5
cobiyu <cobiyu90@gmail.com>
2018-01-09 18:45:16 +0900

    getInitParameter

--- src/com/cobi/Idx.java
+++ src/com/cobi/Idx.java
@@ -6,14 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 //@WebServlet(name = "Idx")
 public class Idx extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("dopost");
+        System.out.println("doget");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doget");
+
+        PrintWriter writer = response.getWriter();
+        String id = getInitParameter("id");
+        writer.println(id);
+        writer.close();
     }
 }
